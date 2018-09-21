1febc209b0df72fd8b3e9e73d277a79b77c8e706
Angelina151612 <Angelina151612@gmail.com>
2018-09-21 10:17:10 +0300

    [CODE READY][TESTS READY]7381 KrevchikAB lab1

--- 7381/KrevchikAB/script.sh
+++ 7381/KrevchikAB/script.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
-gcc main.c -o lr1.o 
+gcc ./Source/main.c -o lr1
 
-cat T1.txt
-./lr1.o < T1.txt
+echo 'Test 1:'
+cat ./Tests/T1.txt
+echo -e '\n'
+./lr1 < ./Tests/T1.txt
 
-cat T2.txt
-./lr1.o < T2.txt
+echo 'Test 2:'
+cat ./Tests/T2.txt
+echo -e '\n'
+./lr1 < ./Tests/T2.txt
 
-cat T3.txt
-./lr1.o < T3.txt
+echo 'Test 3:'
+cat ./Tests/T3.txt
+echo -e '\n'
+./lr1 < ./Tests/T3.txt
 
-cat T4.txt
-./lr1.o < T4.txt
+echo 'Test 4:'
+cat ./Tests/T4.txt
+echo -e '\n'
+./lr1 < ./Tests/T4.txt
 
-cat T5.txt
-./lr1.o < T5.txt
+echo 'Test 5:'
+cat ./Tests/T5.txt
+echo -e '\n'
+./lr1 < ./Tests/T5.txt
+
+echo 'Test 6:'
+cat ./Tests/T6.txt
+echo -e '\n'
+./lr1 < ./Tests/T6.txt
